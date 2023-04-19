d59f2b962a238a346e88578ce1b62f99d8c01179
J. Gabriel González E. <jesusgabriel@10.11.5.5>
2023-04-19 10:50:21 -0600

    Clase del 19 de abril, (Genéricos)

---
+++ src/genericos/Clase.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package genericos;
+
+/**
+ *
+ * @author jesusgabriel
+ */
+public class Clase <tipo> {
+    private tipo a;
+
+    public Clase(tipo t) {
+        a=t;
+    }
+    
+
+    public tipo getA() {
+        return a;
+    }
+
+    public void setA(tipo a) {
+        this.a = a;
+    }
+    
+    public static void main(String[] args) {
+            //debemos poner la clase que representa al tipo primitivo Integer=int;
+        Clase <Integer> obj=new Clase(3);
+            }
+    
+}
