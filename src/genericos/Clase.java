fb7c38e5c192561c6902f39f9436579e61a526de
J. Gabriel González E. <jesusgabriel@10.11.5.5>
2023-04-19 10:53:26 -0600

    Clase del 19 de abril, (Genéricos)

--- src/genericos/Clase.java
+++ src/genericos/Clase.java
@@ -11,22 +11,17 @@ package genericos;
 public class Clase <tipo> {
     private tipo a;
 
-    public Clase(tipo t) {
-        a=t;
-    }
+    public Clase(tipo t) { a=t;}
     
 
-    public tipo getA() {
-        return a;
-    }
+    public tipo getA() {return a;}
 
-    public void setA(tipo a) {
-        this.a = a;
-    }
+    public void setA(tipo a) {this.a = a;}
     
     public static void main(String[] args) {
             //debemos poner la clase que representa al tipo primitivo Integer=int;
         Clase <Integer> obj=new Clase(3);
+        System.out.println(obj.getA());
             }
     
 }
